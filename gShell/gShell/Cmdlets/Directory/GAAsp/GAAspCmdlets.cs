c327913ecf3c5e4fb7aa53806cf555e810172fd6
Spencer Varney <sbvarney@gmail.com>
2015-04-06 12:20:26 -0400

    Added cmdlets for GAAsp and GAChannels for Directory API

---
+++ gShell/gShell/Cmdlets/Directory/GAAsp/GAAspCmdlets.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Data = Google.Apis.Admin.Directory.directory_v1.Data;
+
+namespace gShell.Cmdlets.Directory.GAAsp
+{
+    [Cmdlet(VerbsCommon.Get, "GAAsp",
+          DefaultParameterSetName = "Get",
+          SupportsShouldProcess = true,
+          HelpUri = @"https://github.com/squid808/gShell/wiki/Get-GAAsp")]
+    public class GetGAAsp : DirectoryBase
+    {
+        #region Properties
+
+        [Parameter(Position = 0,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "")]
+        [ValidateNotNullOrEmpty]
+        public string UserKey { get; set; }
+
+        //Domain position = 1
+
+        [Parameter(Position = 2,
+            Mandatory = true,
+            ParameterSetName = "Get",
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "")]
+        [ValidateNotNullOrEmpty]
+        public int CodeId { get; set; }
+
+        [Parameter(Position = 3,
+            Mandatory = true,
+            ParameterSetName = "List",
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "")]
+        public SwitchParameter All { get; set; }
+
+        #endregion
+
+        protected override void ProcessRecord()
+        {
+            if (ShouldProcess(UserKey, "Get-GAAsp"))
+            {
+                switch (ParameterSetName)
+                {
+                    case "OneUser":
+                        WriteObject(asps.Get(UserKey, CodeId));
+                        break;
+                    case "List":
+                        WriteObject(asps.List(UserKey));
+                        break;
+                }
+            }
+        }
+    }
+
+
+    [Cmdlet(VerbsCommon.Remove, "GAAsp",
+          SupportsShouldProcess = true,
+          HelpUri = @"https://github.com/squid808/gShell/wiki/Remove-GAAsp")]
+    public class RemoveGAAsp : DirectoryBase
+    {
+        #region Properties
+
+        [Parameter(Position = 0,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "")]
+        [ValidateNotNullOrEmpty]
+        public string UserKey { get; set; }
+
+        //Domain position = 1
+
+        [Parameter(Position = 2,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "")]
+        [ValidateNotNullOrEmpty]
+        public int CodeId { get; set; }
+
+        [Parameter(Position = 2)]
+        public SwitchParameter Force { get; set; }
+
+        #endregion
+
+        protected override void ProcessRecord()
+        {
+            if (ShouldProcess(UserKey, "Get-GAAsp"))
+            {
+                if (Force || ShouldContinue((String.Format("Asp {0} with CodeID {2} will be removed from the {1} Google Apps domain.\nContinue?",
+                    UserKey, Domain, CodeId)), "Confirm Google Apps User Removal"))
+                {
+                    try
+                    {
+                        WriteDebug(string.Format("Attempting to remove Asp {0}...",
+                            UserKey));
+                        WriteObject(asps.Delete(UserKey, CodeId));
+                        WriteVerbose(string.Format("Removal of Asp {0} completed without error.",
+                            UserKey));
+                    }
+                    catch (Exception e)
+                    {
+                        WriteError(new ErrorRecord(e, e.GetBaseException().ToString(), ErrorCategory.InvalidData, UserKey));
+                    }
+                }
+                else
+                {
+                    WriteError(new ErrorRecord(new Exception("Asp deletion not confirmed"),
+                        "", ErrorCategory.InvalidData, UserKey));
+                }
+            }
+        }
+    }
+}
---
+++ gShell/gShell/Cmdlets/Directory/GAChannel/StopGAChannel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Data = Google.Apis.Admin.Directory.directory_v1.Data;
+
+namespace gShell.Cmdlets.Directory.GAChannel
+{
+    [Cmdlet(VerbsLifecycle.Stop, "GAChannel",
+          SupportsShouldProcess = true,
+          HelpUri = @"https://github.com/squid808/gShell/wiki/Stop-GAChannel")]
+    public class StopGAChannel : DirectoryBase
+    {
+        #region Properties
+
+        [Parameter(
+            Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public string Id { get; set; }
+
+        [Parameter(
+            Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public string ResourceId { get; set; }
+
+        [Parameter()]
+        public SwitchParameter Force { get; set; }
+        
+
+        #endregion
+
+        protected override void ProcessRecord()
+        {
+            if (ShouldProcess("Report Channel", "Stop-GAChannel"))
+            {
+                if (Force || ShouldContinue((String.Format("Resource with Id {0} will be stopped on channel with Id {1}\nContinue?",
+                    ResourceId, Id)), "Confirm Channel Stop"))
+                {
+                    try
+                    {
+                        WriteDebug(string.Format("Attempting to stop Channel Resource..."));
+                        WriteObject(channels.Stop(new Data.Channel(){
+                            Id = Id,
+                            ResourceId = ResourceId
+                        }));
+                        WriteVerbose(string.Format("Channel Resource stopped without error."));
+                    }
+                    catch (Exception e)
+                    {
+                        WriteError(new ErrorRecord(e, e.GetBaseException().ToString(), ErrorCategory.InvalidData, "Stop-GAChannel"));
+                    }
+                }
+                else
+                {
+                    WriteError(new ErrorRecord(new Exception("Stopping of Channel Resource failed"),
+                        "", ErrorCategory.InvalidData, "Stop-GAChannel"));
+                }
+            }
+        }
+    }
+}
