57524f98c334ce6a9206931395ef6b98cdf4d86b
Spencer Varney <squid808@gmail.com>
2016-07-13 20:47:26 -0400

    Added GroupsMigrations and GroupsSettings cmdlet documentations

--- gShell/gShell/Cmdlets/GroupsMigration/GroupsMigration.cs
+++ gShell/gShell/Cmdlets/GroupsMigration/GroupsMigration.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Management.Automation;
-using System.Text;
-
-using Google.Apis.Auth.OAuth2;
-using Data = Google.Apis.GroupsMigration.v1.Data;
-
-using gGroupssettings = gShell.dotNet.GroupsMigration;
 
 namespace gShell.Cmdlets.GroupsMigration
 {
@@ -16,35 +8,78 @@ namespace gShell.Cmdlets.GroupsMigration
         media, resumable
     }
 
+    /// <summary>
+    /// <para type="synopsis">Inserts a new mail into the archive of the Google group.</para>
+    /// <para type="description">Inserts a new mail into the archive of the Google group.</para>
+    /// <list type="alertSet"><item><term>About this Cmdlet</term><description>
+    /// Part of the gShell Project, relating to the Google GroupsMigration API; see Related Links or use the -Online parameter.
+    /// </description></item></list>
+    /// <example>
+    ///   <code>PS C:\>New-GGroupsMigration -GroupUniqueId $SomeGroupIdString</code>
+    ///   <para>This automatically generated example serves to show the bare minimum required to call this Cmdlet.</para>
+    ///   <para>Additional examples may be added, viewed and edited by users on the community wiki at the URL found in the related links.</para>
+    /// </example>
+    /// <example>
+    ///   <code>PS C:\>New-GGroupsMigration -GroupUniqueId $SomeGroupIdString -FilePath $SomeFilePath -UploadType media</code>
+    ///   <para>This automatically generated example serves to show the bare minimum required to call this Cmdlet.</para>
+    ///   <para>Additional examples may be added, viewed and edited by users on the community wiki at the URL found in the related links.</para>
+    /// </example>
+    /// <para type="link" uri="https://github.com/squid808/gShell/wiki/New-GGroupsMigration">[Wiki page for this Cmdlet]</para>
+    /// <para type="link" uri="https://github.com/squid808/gShell/wiki/Getting-Started">[Getting started with gShell]</para>
+    /// </summary>
     [Cmdlet(VerbsCommon.New, "GGroupsMigration",
         SupportsShouldProcess = true,
-          HelpUri = @"")]
+          HelpUri = @"https://github.com/squid808/gShell/wiki/New-GGroupsMigration",
+          DefaultParameterSetName = "NoUpload")]
     public class NewGGroupsMigration : GroupsMigrationBase
     {
         #region Properties
+
+        /// <summary>
+        /// <para type="description">The group ID</para>
+        /// </summary>
         [Parameter(Position = 0,
-            Mandatory = true)]
+            Mandatory = true,
+            HelpMessage = "The group ID")]
         [ValidateNotNullOrEmpty]
         public string GroupId { get; set; }
 
+        /// <summary>
+        /// <para type="description">The path to the file to upload.</para>
+        /// </summary>
         [Parameter(Position = 2,
-            Mandatory = true)]
+            Mandatory = true,
+            HelpMessage = "The path to the file to upload.",
+            ParameterSetName = "MediaUpload")]
         [ValidateNotNullOrEmpty]
         public string FilePath { get; set; }
 
+        /// <summary>
+        /// <para type="description">The type of upload request to the upload URI.</para>
+        /// </summary>
         [Parameter(Position = 3,
-            Mandatory = true)]
+            Mandatory = true,
+            HelpMessage = "The type of upload request to the upload URI",
+            ParameterSetName = "MediaUpload")]
         [ValidateNotNullOrEmpty]
         public UploadTypeEnum UploadType { get; set; }
+
         #endregion
 
         protected override void ProcessRecord()
         {
             if (ShouldProcess("GroupsMigration", "New-GGroupsMigration"))
             {
-                using (FileStream stream = File.OpenRead(FilePath))
+                if (ParameterSetName == "Default")
                 {
-                    archive.Insert(GroupId, stream, UploadType.ToString());
+                    WriteObject(archive.Insert(GroupId));
+                }
+                else
+                {
+                    using (FileStream stream = File.OpenRead(FilePath))
+                    {
+                        archive.Insert(GroupId, stream, UploadType.ToString());
+                    }
                 }
             }
         }
